2a0f22df1107ebfd9eef2b649efd966f5a21b7b1
Andrew Kislitsyn <kisandy@yandex.ru>
2019-10-08 19:17:32 +0300

    Update siganture

--- src/main/kotlin/signature.kt
+++ src/main/kotlin/signature.kt
@@ -1,50 +1,42 @@
-
-//Сервер, хранит все базы данных, принимает запросы клиента и отвечает на них
-
 class Server {
+    var chatBase: ChatBase? = null
+    var userBase: UserBase? = null
+    var messageBase: MessageBase? = null
 
-    // эти датабазы - оболочки над человческими
-
-    var chatBase : ChatBase? = null
-    var userBase : UserBase? = null
-    // var attachmentBase : AttachmentBase = null
-    var messageBase : MessageBase? = null
-
-    fun request(){} // URL -> JSON
+    fun answerRequest(request : ServerRequest) {}
 }
 
-// Клиент, обменивается ифнормацией с сервером, отвечает за UI-шку
 
 class Client(val server: Server) {
-    var loggedUserId : Long = -1 // id залогиненного юзера
+    var loggedUserId: Long = -1 // id залогиненного юзера
 
-    //тут должен быть чел, которые делает запрос на сервер и который парсит ответы на них
+    private val objectMapper = jacksonObjectMapper()
 
-    //все функции - запросы вида клиент -> сервер -> клиент
-
-    fun login() {}// авторизация
-    fun register(){} // регистрация
-}
-
-
-//
-// пока что консольный с дефолтными командами, пока не реализоывваем
-class UI(val client: Client){
+    class userDataGetter(val userId: Long)
+    class userDataSetter(val userId: Long)
+    class messageDataGetter(val messageId: Long)
+    class messageDataSetter(val messageId: Long)
+    class chatDataGetter(val chatId: Long)
+    class chatDataSetter(val chatId: Long)
 
 }
 
-// база даннных сначала испоользуем мапы, в дальнейшем перейдём на номральные DB. Сервер будет уметь с ними работать
-// и по запросу, например юзера, берёт ифну из DB и возварщает JSON
+
+class UI(val client: Client) {
+
+}
+
 
 class ChatBase {
-    private val chats : MutableMap<Long, Chat>? = null // Map(id, chat)
-    private var baseSize : Int = 0 //  кол-во чатов
+    private val chats: MutableMap<Long, Chat>? = null // Map(id, chat)
+    private var baseSize: Int = 0 //  кол-во чатов
     fun getChat(id: Int) {} // возвращает чат по id
     fun addChat() {} // добавляет чат
 }
 
+
 class UserBase {
-    val users : MutableMap<Long, User>? = null // Map(id, chat)
+    val users: MutableMap<Long, User>? = null // Map(id, chat)
     fun searchByName() {}
     fun searchById() {}
     fun addUser() {}
@@ -52,89 +44,56 @@ class UserBase {
 
 }
 
+
 class MessageBase {
-    val messages : MutableMap<Long, Message>? = null
+    val messages: MutableMap<Long, Message>? = null
     fun getChat(id: Int) {}
 }
 
-// User - основной класс, в котором хранятся все его данные, а также имеет все необходимые методы
-// Основной конструктор - по JSON - у
 
-class User {
-
-    val blockedUsers : List<Long>
-        get() {
-            TODO()
-        }
-
-    val userChatsId : List<Long>
-        get() {
-            TODO()
-        }
-
-    lateinit var name : String
-    val contacts : Map<Long, String>
-        get() {
-            TODO()
-        }
-    fun createChat() {}
-    fun addChat() {}
-    fun removeChat() {}
-    fun sendMessage() {}
-    fun deleteMessage() {}
-    fun findMessage () {}
-    fun leaveChat() {}
-}
-
-//пока забить
-class AttachmentBase {
-    val Attachments : MutableMap<Long, Attachment>
-        get() {
-            TODO()
-        }
-
-    fun Download() {}
-}
-
-class Attachment {
-    val type : String = ""
+data class User(val userId: Long, val login: String) {
+    var name: String = ""
+    val blockedUsers: MutableList<Long> = mutableListOf()
+    val userChatsId: List<Long> = mutableListOf()
+    val contacts: MutableMap<Long, String> = mutableMapOf() // id, имя контакта
 }
 
 
-class Message {
-    val idInChat : Long = -1
-    var isEdited : Boolean = false
-    var isRead : Boolean = false
-    var isSent : Boolean = false
-    var isDeleted : Boolean = false
-    // val messageAttachments : List<Attachment>
-    var text : String = ""
-    val autherId : Long = -1
+data class Message(var text: String, val chatId: Long, val userId: Long) {
+    var isEdited: Boolean = false
+    var isRead: Boolean = false
+    var isSent: Boolean = false
+    var isDeleted: Boolean = false
+
+    //val attachment_ids : MutableList<Long>
+
+    /*val idInChat: Long
+    init {
+        idInChat = TODO()
+    }*/
 }
+
 
 interface Chat {
+    var members: MutableSet<Long>
+    val messages: MutableList<Message>
+    val id: Long
+
     //var isPrivate : Boolean
-    val Messages : List<Message>
-    val Id : Long
-    //fun SendNotification() {}
-    //fun Attach() {}
 }
 
-/*
-class GroupChat : Chat {
-    var Members : MutableMap<User>
-    val chatOwner : User
-    var Name : String
-    var Reference : String
-    fun AddMember() {}
-    fun DeleteMember() {}
+
+data class GroupChat(override val id: Long, val chatOwner: Long, var Name: String) : Chat {
+
+    override var members: MutableSet<Long> = mutableSetOf(chatOwner)
+    override val messages: MutableList<Message> = mutableListOf()
+
+    //var Reference : String
 }
-*/
 
 
-//Пофиксите пожалуйста что тут он ругается
-class singleChat : Chat {
-    val user1 : User
-    val user2 : User
+data class singleChat(override val id: Long, val user1: Long, val user2: Long) : Chat {
+    override var members: MutableSet<Long> = mutableSetOf(user1, user2)
+    override val messages: MutableList<Message> = mutableListOf()
 }
 
