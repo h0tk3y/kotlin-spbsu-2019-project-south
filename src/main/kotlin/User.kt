d9103980091e5bff14fedd482612033f86f949bd
Ilya Voronin <voroninilya115@gmail.com>
2019-10-11 22:32:05 +0300

    rename userId to id

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,4 +1,4 @@
-data class User(val userId: Long, val login: String) {
+data class User(val id: Long, val login: String) {
     var name: String = ""
     val blockedUsers: MutableList<Long> = mutableListOf()
     val userChatsId: List<Long> = mutableListOf()
