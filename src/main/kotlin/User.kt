fb6f889ae01b25768ef4df2261cb109068bd472b
Ilya Voronin <voroninilya115@gmail.com>
2019-10-11 17:49:16 +0300

    add User data class

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,2 +1,6 @@
-class User {
+data class User(val id: Long, val login: String) {
+    var name: String = ""
+    val blockedUsers: MutableList<Long> = mutableListOf()
+    val userChatsId: List<Long> = mutableListOf()
+    val contacts: MutableMap<Long, String> = mutableMapOf() // id, имя контакта
 }
