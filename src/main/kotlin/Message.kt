8d9df645b04335fc4f73ed0de28fc65220fa49e4
Gleb Oborin <oborin.gleb@yandex.ru>
2019-10-11 17:16:58 +0300

    message implementation complete

--- src/main/kotlin/Message.kt
+++ src/main/kotlin/Message.kt
@@ -1,13 +1,6 @@
-data class Message(var text : String, val chatId : Long, val userId : Long) {
+data class Message(var text : String, val chatId : Long, val userId : Long, val idInChat : Long) {
     var isEdited: Boolean = false
     var isRead: Boolean = false
     var isSent: Boolean = false
     var isDeleted: Boolean = false
-
-    val attachment_ids : MutableList<Long>
-
-    val idInChat: Long
-    init {
-        idInChat = TODO()
-    }
 }
