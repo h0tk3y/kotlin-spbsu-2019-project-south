23760a1d71f34209de528be0f7c3cbf6eb0fb680
Ilya Voronin <voroninilya115@gmail.com>
2019-10-12 11:42:20 +0300

    add bassic methods for Client.UserData

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,13 +1,47 @@
 import com.fasterxml.jackson.databind.util.JSONPObject
 import com.fasterxml.jackson.module.kotlin.`KotlinPackage-Extensions-6da2b0d6`.jacksonObjectMapper
-
+import ReqType.*
+import FieldType.*
 
 class Client() {
     var loggedUserId: Long = -1
 
     private val objectMapper = jacksonObjectMapper()
 
-    class userData(val userId: Long)
+    class userData(val userId: Long) {
+        private fun getUser(id : Long) : User {
+            return objectMapper.readValue<User>(ServerRequest(GET, USER, userId).makeRequest())
+        }
+        private fun editUser(user : User) {
+            ServerRequest(EDIT, USER, userId, user).makeRequest()
+        }
+        fun addContact(contactId: Long, name : String = ""){
+            val cur = getUser(userId);
+            if (name == "") {
+                val newUser = getUser(contactId)
+                cur.contacts[contactId] = newUser.name
+            }
+            else {
+                cur.contacts[contactId] = name
+            }
+            editUser(cur)
+        }
+        fun addBlockedUser(blockedUserId: Long){
+            val cur = getUser(userId)
+            cur.blockedUsers.add(blockedUserId)
+            editUser(cur)
+        }
+        fun addChat(chatId : Long){
+            val cur = getUser(userId)
+            cur.chatsId.add(chatId)
+            editUser(cur)
+        }
+        fun changeName(newName : String) {
+            val cur = getUser(userId)
+            cur.name = newName
+            editUser(cur)
+        }
+    }
     class messageData(val messageId: Long)
     class chatData(val chatId: Long)
 }
--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,6 +1,6 @@
 data class User(val id: Long, val login: String) {
     var name: String = ""
     val blockedUsers: MutableList<Long> = mutableListOf()
-    val userChatsId: List<Long> = mutableListOf()
+    val chatsId: MutableList<Long> = mutableListOf()
     val contacts: MutableMap<Long, String> = mutableMapOf() // id, имя контакта
 }
