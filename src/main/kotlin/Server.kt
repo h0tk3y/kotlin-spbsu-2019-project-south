680a97264f09ddd0a307f48c05d2bd3ee0f59ddc
handlessmidas <midas1024@yandex.ru>
2019-10-13 02:44:26 +0300

    Implemeted simple ktor server

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,5 +1,15 @@
 import com.fasterxml.jackson.databind.util.JSONPObject
 import com.fasterxml.jackson.module.kotlin.*
+import io.ktor.application.install
+import io.ktor.features.AutoHeadResponse.install
+import io.ktor.http.cio.websocket.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.websocket.WebSockets
+import io.ktor.websocket.webSocket
+import java.net.http.HttpClient
+
 class Server {
     private val objectMapper = jacksonObjectMapper()
 
@@ -7,9 +17,7 @@ class Server {
     private var chatBase : ChatBase = ChatBase("chats.db")
     private var messageBase : MessageBase = MessageBase("messages.db")
 
-    fun answerRequest(requestString: String) : String {
-        val request: ServerRequest = ServerRequest(requestString)
-
+    fun processRequest(request: ServerRequest) : String {
         when (request.fieldType) {
             FieldType.USER -> {
                 val user: User = objectMapper.readValue<User>(request.body)
@@ -40,6 +48,27 @@ class Server {
             }
         }
         return ""
+    }
+
+    fun answerRequest(port : Int = 9999) {
+        embeddedServer(Netty, port) {
+            install(WebSockets)
+            routing {
+                webSocket {
+                    while (true) {
+                        val frame = incoming.receive()
+                        if (frame is Frame.Text) {
+                            val text = frame.readText()
+                            val request : ServerRequest? = ServerRequest(text)
+                            if (request != null) {
+                                val response = processRequest(request)
+                                outgoing.send(Frame.Text(response))
+                            }
+                        }
+                    }
+                }
+            }
+        }.start(wait = true)
     }
 }
 
