ddab3cecc730a11d8cf84ed242b4f549a4796f9b
handlessmidas <midas1024@yandex.ru>
2019-10-14 15:54:12 +0300

    Fixed Server

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,6 +1,5 @@
 import com.fasterxml.jackson.module.kotlin.*
 import io.ktor.application.install
-import io.ktor.features.AutoHeadResponse.install
 import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
@@ -15,37 +14,38 @@ class Server {
     private var chatBase : ChatBase = ChatBase("chats.db")
     private var messageBase : MessageBase = MessageBase("messages.db")
 
-    private fun processRequest(request: ServerRequest) : String {
+    private fun processRequest(request: ServerRequest) : ServerResponse {
+        val response = ServerResponse(request.requestType, request.fieldType)
         when (request.fieldType) {
             FieldType.USER -> {
                 val user: User = objectMapper.readValue(request.body)
                 when (request.requestType) {
-                    RequestType.GET -> return objectMapper.writeValueAsString(userBase.get(user.id))
-                    RequestType.ADD -> userBase.add(user)
-                    RequestType.REMOVE -> userBase.remove(user.id)
-                    RequestType.EDIT -> userBase.edit(user.id, user)
+                    TransportType.GET -> response.body = objectMapper.writeValueAsString(userBase.get(user.id))
+                    TransportType.ADD -> userBase.add(user)
+                    TransportType.REMOVE -> userBase.remove(user.id)
+                    TransportType.EDIT -> userBase.edit(user.id, user)
                 }
             }
             FieldType.CHAT -> {
                 val chat: Chat = objectMapper.readValue(request.body)
                 when (request.requestType) {
-                    RequestType.GET -> return objectMapper.writeValueAsString(chatBase.get(chat.id))
-                    RequestType.ADD -> chatBase.add(chat)
-                    RequestType.REMOVE -> chatBase.remove(chat.id)
-                    RequestType.EDIT -> chatBase.edit(chat.id, chat)
+                    TransportType.GET -> response.body = objectMapper.writeValueAsString(chatBase.get(chat.id))
+                    TransportType.ADD -> chatBase.add(chat)
+                    TransportType.REMOVE -> chatBase.remove(chat.id)
+                    TransportType.EDIT -> chatBase.edit(chat.id, chat)
                 }
             }
             FieldType.MESSAGE -> {
                 val message: Message = objectMapper.readValue(request.body)
                 when (request.requestType) {
-                    RequestType.GET -> return objectMapper.writeValueAsString(messageBase.get(message.id))
-                    RequestType.ADD -> messageBase.add(message)
-                    RequestType.REMOVE -> messageBase.remove(message.id)
-                    RequestType.EDIT -> messageBase.edit(message.id, message)
+                    TransportType.GET -> response.body = objectMapper.writeValueAsString(messageBase.get(message.id))
+                    TransportType.ADD -> messageBase.add(message)
+                    TransportType.REMOVE -> messageBase.remove(message.id)
+                    TransportType.EDIT -> messageBase.edit(message.id, message)
                 }
             }
         }
-        return ""
+        return response
     }
 
     fun respondToRequest(port : Int = 9999) {
@@ -61,7 +61,7 @@ class Server {
                             val request : ServerRequest? = objectMapper.readValue(text)
                             if (request != null) {
                                 val response = processRequest(request)
-                                outgoing.send(Frame.Text(response))
+                                outgoing.send(Frame.Text(objectMapper.writeValueAsString(response)))
                             }
                         }
                     }
