12361d7fedfc74678787e51ab86e61282ab29ca5
Gleb Oborin <oborin.gleb@yandex.ru>
2019-10-18 21:16:59 +0300

    fixed codestyle

--- src/main/kotlin/ServerResponse.kt
+++ src/main/kotlin/ServerResponse.kt
@@ -1,20 +1,13 @@
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
-class ServerResponse {
-
-    constructor (responseType: TransportType? = null, fieldType: FieldType? = null, id: Long = -1, body : String = "") {
-        this.id = id
-        this.responceType = responseType
-        this.fieldType = fieldType
-        this.body = body
-    }
+class ServerResponse(
+    var responseType: TransportType? = null,
+    var fieldType: FieldType? = null,
+    var id: Long = -1,
+    var body: String = ""
+) {
 
     private val objectMapper = jacksonObjectMapper()
-
-    var id : Long = -1
-    var responceType : TransportType? = null
-    var fieldType : FieldType? = null
-    var body : String = ""
 
     override fun toString(): String {
         return objectMapper.writeValueAsString(this)
