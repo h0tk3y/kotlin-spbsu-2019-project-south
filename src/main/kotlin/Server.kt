5a3ae52cb7c0def9192a3957b8c976b5626b8616
handlessmidas <midas1024@yandex.ru>
2019-10-27 18:51:13 +0300

    Added find request

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -24,6 +24,7 @@ class Server {
                     TransportType.ADD -> userBase.add(user)
                     TransportType.REMOVE -> userBase.remove(user.id)
                     TransportType.EDIT -> userBase.edit(user.id, user)
+                    TransportType.FIND -> response.body = objectMapper.writeValueAsString(userBase.find(user.id))
                 }
             }
             FieldType.CHAT -> {
@@ -33,6 +34,7 @@ class Server {
                     TransportType.ADD -> chatBase.add(chat)
                     TransportType.REMOVE -> chatBase.remove(chat.id)
                     TransportType.EDIT -> chatBase.edit(chat.id, chat)
+                    TransportType.FIND -> response.body = objectMapper.writeValueAsString(chatBase.find(chat.id))
                 }
             }
             FieldType.MESSAGE -> {
@@ -42,6 +44,7 @@ class Server {
                     TransportType.ADD -> messageBase.add(message)
                     TransportType.REMOVE -> messageBase.remove(message.id)
                     TransportType.EDIT -> messageBase.edit(message.id, message)
+                    TransportType.FIND -> response.body = objectMapper.writeValueAsString(messageBase.find(message.id))
                 }
             }
         }
