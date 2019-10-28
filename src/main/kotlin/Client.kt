0e981449a3ee02983631b19044e9c14303510713
Andrew Kislitsyn <kisandy@yandex.ru>
2019-10-28 14:40:22 +0300

    fix client style

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -3,9 +3,10 @@ import TransportType.*
 import FieldType.*
 
 object Client {
-    private var loggedUserId: Long = -1
-
     val webClient = WebClient("127.0.0.1", 9999)
+    private val objectMapper = jacksonObjectMapper()
+
+    private var loggedUserId: Long = -1
 
     fun getLoggedUserId(): Long {
         return loggedUserId
@@ -16,7 +17,6 @@ object Client {
     }
 
     class UserData(private var userId: Long = loggedUserId) {
-        private val objectMapper = jacksonObjectMapper()
 
         private fun getUser(id: Long): User {
             return objectMapper.readValue(
@@ -113,7 +113,6 @@ object Client {
     }
 
     class MessageData(private val messageId: Long = -1) {
-        private val objectMapper = jacksonObjectMapper()
 
         private fun getMessage(): Message =
             objectMapper.readValue(
@@ -172,7 +171,6 @@ object Client {
     }
 
     class ChatData(private val chatId: Long = -1) {
-        private val objectMapper = jacksonObjectMapper()
 
         private fun getChat(): Chat {
             return objectMapper.readValue(webClient.makeRequest(ServerRequest(GET, CHAT, chatId)).body)
