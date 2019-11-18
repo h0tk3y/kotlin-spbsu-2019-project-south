aa72965d3d93971b726705db79c9e2678a35b1dd
handlessmidas <midas1024@yandex.ru>
2019-11-18 16:20:00 +0300

    Added RequestHandler

--- src/main/kotlin/Transport/RequestHandler.kt
+++ src/main/kotlin/Transport/RequestHandler.kt
@@ -1,88 +1,137 @@
+import DataBases.DataBaseHandler
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import java.lang.reflect.Member
 
-class RequestHandler {
+class RequestHandler() {
     private val objectMapper = jacksonObjectMapper()
+    private val handler = DataBaseHandler()
+    private var userBase: UserBase
+    private var chatBase: ChatBase
+    private var messageBase: MessageBase
 
-    private var userBase : UserBase = UserBase("users.db")
-    private var chatBase : ChatBase = ChatBase("chats.db")
-    private var messageBase : MessageBase = MessageBase("messages.db")
+
+    init {
+        handler.initAll()
+        this.userBase = UserBase(this.handler.connection!!)
+        this.chatBase = ChatBase(this.handler.connection!!)
+        this.messageBase = MessageBase(this.handler.connection!!)
+    }
 
 
-    fun getUser(userId : Long) : User? {
+
+
+    fun getUser(userId: Long): User? {
         return userBase.get(userId)
     }
+
+    fun getUserChats(userId: Long): MutableMap<Long, String> {
+        return userBase.getChats(userId)
+    }
+
+    fun getUserContacts(userId: Long): MutableMap<Long, String> {
+        return userBase.getContacts(userId)
+    }
+
+    fun removeUser(chatId: Long, userIdString: String) {
+        val userId: Long = objectMapper.readValue(userIdString)
+        chatBase.removeMember(chatId, userId)
+    }
+
+    fun editUser(userId: Long, userString: String) {
+        val user: User = objectMapper.readValue(userString)
+        userBase.edit(userId, user)
+    }
+
+    fun removeContact(userId: Long, contactString: String) {
+        val contact: User = objectMapper.readValue(contactString)
+        userBase.removeContact(userId, contact.id, contact.name)
+    }
+
+    fun addContact(userId: Long, contactString: String) {
+        val contact: User = objectMapper.readValue(contactString)
+        userBase.addContact(userId, contact.id, contact.name)
+    }
+
+
+
 
     fun sendMessage(messageString : String) {
         val message : Message = objectMapper.readValue(messageString)
         messageBase.add(message)
     }
 
-    fun getChat(chatId : Long) : Chat? {
-        return chatBase.get(chatId)
-    }
-
-    fun getMessages(chatId : Long, countString : String) : List<Message>{
-        val count : Long = objectMapper.readValue(countString)
-        return chatBase.getMessages(chatId, count)
-    }
-
-    fun deleteMessage(MessageId : Long) {
-        messageBase.remove(MessageId)
-    }
-
-    fun deleteChat(chatId : Long) {
-        chatBase.remove(chatId)
-    }
-
-    fun editMessage(messageId : Long, messageString : String) {
-        val message : Message = objectMapper.readValue(messageString)
+    fun editMessage(messageId: Long, messageString: String) {
+        val message: Message = objectMapper.readValue(messageString)
         messageBase.edit(messageId, message)
     }
 
-    fun editUser(userId : Long, userString : String) {
-        val user : User = objectMapper.readValue(userString)
-        userBase.edit(userId, user)
+    fun deleteMessage(messageId: Long) {
+        messageBase.remove(messageId)
     }
 
-    fun getUserChats(userId : Long) : List<Chat> {
-        return chatBase.getChats(userId)
+    fun getMessage(messageId: Long): Message? {
+        return messageBase.get(messageId)
     }
 
-    fun addChat(chatString : String) {
-        val chat : Chat = objectMapper.readValue(chatString)
+
+
+
+    fun addChat(chatString: String) {
+        val chat: Chat = objectMapper.readValue(chatString)
         chatBase.add(chat)
     }
 
-    fun removeUser(chatId : Long, userIdString : String) {
-        val userId : Long = objectMapper.readValue(userIdString)
-        var chat = chatBase.get(chatId)
-        chat?.members?.remove(userId)
-        if (chat != null) {
-            chatBase.edit(chatId, chat)
-        }
+    fun getChat(chatId: Long): Chat? {
+        return chatBase.get(chatId)
     }
 
-    fun editChat(chatId : Long, chatString : String) {
-        val chat : Chat = objectMapper.readValue(chatString)
+    fun getMessages(chatId: Long): MutableSet<Message>{
+        return chatBase.getMessages(chatId)
+    }
+
+    fun getMembers(chatId: Long): MutableSet<Long> {
+        return chatBase.getMembers(chatId)
+    }
+
+    fun getAdmins(chatId: Long): MutableSet<Long> {
+        return chatBase.getAdmins(chatId)
+    }
+
+    fun deleteChat(chatId: Long) {
+        chatBase.remove(chatId)
+    }
+
+    fun editChat(chatId: Long, chatString: String) {
+        val chat: Chat = objectMapper.readValue(chatString)
         chatBase.edit(chatId, chat)
     }
 
-    fun addMember(chatId : Long, memberString : String) {
-        val member : User = objectMapper.readValue(memberString)
-        val chat = chatBase.get(chatId)
-        chat?.members?.add(member.id)
-        if (chat != null) {
-            chatBase.edit(chatId, chat)
-        }
+    fun addMember(chatId: Long, memberIdString: String) {
+        val memberId: Long = objectMapper.readValue(memberIdString)
+        chatBase.addMember(chatId, memberId)
     }
 
-    fun addAdmin(chatId : Long, memberString : String) {
-        val member : User = objectMapper.readValue(memberString)
-        val chat = chatBase.get(chatId)
-        chat?.owners?.add(member.id)
-        if (chat != null) {
-            chatBase.edit(chatId, chat)
-        }
+    fun kickMember(chatId: Long, memberIdString: String) {
+        val memberId: Long = objectMapper.readValue(memberIdString)
+        chatBase.removeMember(chatId, memberId)
+    }
+
+    fun addAdmin(chatId: Long, adminIdString: String) {
+        val adminId: Long = objectMapper.readValue(adminIdString)
+        chatBase.addAdmin(chatId, adminId)
+    }
+
+    fun removeAdmin(chatId: Long, adminIdString: String) {
+        val adminId: Long = objectMapper.readValue(adminIdString)
+        chatBase.removeAdmin(chatId, adminId)
+    }
+
+
+
+
+    fun register(userString: String) {
+        val user: User = objectMapper.readValue(userString)
+        userBase.add(user)
     }
 }
