6c288bf64d298ffff4babb0d7a7d18836a9ac50e
Andrew Kislitsyn <kisandy@yandex.ru>
2019-11-25 14:29:25 +0300

    add comments for types of request

--- src/main/kotlin/Transport/TransportType.kt
+++ src/main/kotlin/Transport/TransportType.kt
@@ -1,31 +1,32 @@
 enum class TransportType {
 
-    GET_USER,
-    GET_USER_CHATS,
-    REMOVE_USER,
-    EDIT_USER,
-    ADD_CONTACT,
-    EDIT_CONTACT,
-    REMOVE_CONTACT,
-    BLOCK_USER,
-    UNBLOCK_USER,
-    GET_BLOCKED_USERS,
-    GET_CONTACTS,
+    GET_USER,        //get user by id
+    GET_USER_CHATS,  //return all user chats map (ids, name)
+    REMOVE_USER,    // todo
+    EDIT_USER,      //edit user data
+    ADD_CONTACT,    //add contact connection between users
+    EDIT_CONTACT,   //edit contact
+    REMOVE_CONTACT, // todo
+    BLOCK_USER,    // block connection
+    UNBLOCK_USER,  // unblock connection
+    GET_BLOCKED_USERS, // return all user blocked users map (id, name)
+    GET_CONTACTS, // return all user contacts map (id, name)
 
     SEND_MESSAGE,
     EDIT_MESSAGE,
     DELETE_MESSAGE,
-    GET_MESSAGE,
+    GET_MESSAGE, // return message by id
 
-    ADD_CHAT,
-    GET_CHAT,
-    GET_MESSAGES,
-    GET_MEMBERS,
-    GET_ADMINS,
-    DELETE_CHAT,
+
+    ADD_CHAT, // create new chat
+    GET_CHAT, // get chat by id
+    GET_MESSAGES, // return list of messages
+    GET_MEMBERS, // return list of members id
+    GET_ADMINS, // return list of members id
+    DELETE_CHAT, // todo
     EDIT_CHAT,
-    ADD_MEMBER,
-    JOIN_CHAT,
+    ADD_MEMBER, // adding(inviting) new member to chat
+    JOIN_CHAT, // join to chat
     KICK_MEMBER,
     LEAVE_CHAT,
     ADD_ADMIN,
