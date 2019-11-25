40fe34052d5f1bc6b62d61a4170823b5c3e3cc98
Andrew Kislitsyn <kisandy@yandex.ru>
2019-11-25 14:49:09 +0300

    add new tt + access for all types

--- src/main/kotlin/Transport/TransportType.kt
+++ src/main/kotlin/Transport/TransportType.kt
@@ -1,37 +1,39 @@
 enum class TransportType {
 
-    GET_USER,        //get user by id
-    GET_USER_CHATS,  //return all user chats map (ids, name)
+    GET_USER,        //get user by id (access for all)
+    GET_USER_CHATS,  //return all user chats map (ids, name) (access only for target user | ACCESS ERROR)
     REMOVE_USER,    // todo
-    EDIT_USER,      //edit user data
-    ADD_CONTACT,    //add contact connection between users
-    EDIT_CONTACT,   //edit contact
+    EDIT_USER,      //edit user data (access only for target user | ACCESS ERROR)
+    ADD_CONTACT,    //add contact connection between users (access only for not blocked users |ACCESS ERROR | BL ERROR)
+    EDIT_CONTACT,   //edit contact (access only for target user) (ACCESS ERROR)
     REMOVE_CONTACT, // todo
-    BLOCK_USER,    // block connection
-    UNBLOCK_USER,  // unblock connection
-    GET_BLOCKED_USERS, // return all user blocked users map (id, name)
-    GET_CONTACTS, // return all user contacts map (id, name)
+    BLOCK_USER,    // block connection (access only for target user | ACCESS ERROR | STATUS ERROR)
+    UNBLOCK_USER,  // unblock connection (access only for target user | ACCESS ERROR | STATUS ERROR)
+    GET_BLOCKED_USERS, // return all user blocked users map (id, name) (access only for target user | ACCESS ERROR)
+    GET_CONTACTS, // return all user contacts map (id, name) (access only for target user | ACCESS ERROR)
 
-    SEND_MESSAGE,
-    EDIT_MESSAGE,
-    DELETE_MESSAGE,
-    GET_MESSAGE, // return message by id
+    SEND_MESSAGE, // (access only for chat members | ACCESS ERROR)
+    EDIT_MESSAGE, // (access only for author | ACCESS ERROR)
+    DELETE_MESSAGE, //(access only for author and admins | ACCESS ERROR)
+    GET_MESSAGE, // return message by id (access only for chat members | ACCESS ERROR)
 
 
-    ADD_CHAT, // create new chat
-    GET_CHAT, // get chat by id
-    GET_MESSAGES, // return list of messages
-    GET_MEMBERS, // return list of members id
-    GET_ADMINS, // return list of members id
+    ADD_CHAT, // create new chat (access for all)
+    GET_CHAT, // todo
+    GET_MESSAGES, // return list of messages (access only for chat members | ACCESS ERROR)
+    GET_MEMBERS, // return list of members id (access only for admins | ACCESS ERROR)
+    GET_ADMINS, // return list of members id (access only for admins (mb for members - todo) | ACCESS ERROR)
     DELETE_CHAT, // todo
-    EDIT_CHAT,
-    ADD_MEMBER, // adding(inviting) new member to chat
-    JOIN_CHAT, // join to chat
-    KICK_MEMBER,
-    LEAVE_CHAT,
-    ADD_ADMIN,
-    REMOVE_ADMIN,
+    EDIT_CHAT, // (access only for admins | ACCESS ERROR)
+    ADD_MEMBER, // adding(inviting) new member to chat (hz dlya kogo access todo)
+    JOIN_CHAT, // join to chat (access for not blocked in chat user | ACCESS ERROR | STATUS ERROR)
+    KICK_MEMBER, // (access for admin | ACCESS ERROR | STATUS ERROR)
+    LEAVE_CHAT, // (access for members | ACCESS ERROR | STATUS ERROR)
+    ADD_ADMIN, // (access for owner | ACCESS ERROR | STATUS ERROR)
+    REMOVE_ADMIN, // (access for owner | ACCESS ERROR | STATUS ERROR)
+    BLOCK_USER_IN_CHAT, // (access for admin | ACCESS ERROR | STATUS ERROR)
+    UNBLOCK_USER_IN_CHAT, // (access for admin | ACCESS ERROR | STATUS ERROR)
 
-    REGISTER,
-    LOGIN
+    REGISTER, // (access for all)
+    LOGIN    // c(access for all)
 }
