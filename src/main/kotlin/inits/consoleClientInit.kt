ee01539c0e83914e9ab836b96eaf887f439a103d
Ilya Voronin <voroninilya115@gmail.com>
2019-12-09 12:18:14 +0300

    add Client init

--- src/main/kotlin/inits/consoleClientInit.kt
+++ src/main/kotlin/inits/consoleClientInit.kt
@@ -1,10 +1,19 @@
-/*
-fun main() {
-    GlobalScope.launch {
-        Client.webClient.run()
-    }
-    consoleUi.LoginMenu().mainAction()
-    consoleUi.MainMenu().mainAction()
-}
+import client.Client
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
- */
+fun main() {
+    GlobalScope.launch { Client.webClient.run() }
+    try {
+        Client.LoginDataHandler().registerUser("aba", "aba", "aba", "aba")
+    } catch (e : client.ServerException) {
+        println(e.message)
+    }
+    try {
+        Client.LoginDataHandler().registerUser("ada", "ada", "ada", "ada")
+    } catch (e : client.ServerException) {
+        println(e.message)
+    }
+    println(555555)
+}
